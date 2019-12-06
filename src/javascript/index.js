da918a9d42a4801f6c8d56c438e496e50f59db32
jcklpe <david@jackalope.tech>
2019-12-06 13:52:14 -0600

    updated with some better file structure and a slightly different build structure

--- src/javascript/index.js
+++
@@ -1,9 +0,0 @@
-// import "@getbase/base";
-// import "@getbase/buttons";
-// import "@getbase/containers";
-// import "@getbase/forms";
-// import "@getbase/grid";
-// import "@getbase/tables";
-// import "@getbase/typography";
-import "skeleton-sass-official";
-import "../scss/styles.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
-// Webpack uses this to work with directories
 const path = require("path");
+
+// include the js minification plugin
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+// include the css extraction and minification plugins
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-// This is main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: "./src/javascript/index.js",
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+  entry: {
+    headjs: "./assets/src/js/head.js",
+    footerjs: "./assets/src/js/footer.js",
+    styles: "./assets/src/scss/styles.scss"
   },
-
+  output: {
+    filename: "./assets/build/[name].min.js",
+    path: path.resolve(__dirname)
+  },
+  devtool: "source-map",
   module: {
     rules: [
-      //- Javascript loader
+      // perform js babelization on all .js files
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
@@ -28,82 +31,45 @@ module.exports = {
           }
         }
       },
-      //- SCSS loader
+      //Compile SCSS to css
       {
-        // Apply rule for .sass, .scss or .css files
-        test: /\.(sa|sc|c)ss$/,
-
-        // Set loaders to transform files.
-        // Loaders are applying from right to left(!)
-        // The first loader will be applied after others
+        test: /\.(sass|scss)$/,
+        // use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         use: [
+          MiniCssExtractPlugin.loader,
           {
-            // After all CSS loaders we use plugin to do his work.
-            // It gets all transformed CSS and extracts it into separate
-            // single bundled file
-            loader: MiniCssExtractPlugin.loader
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
           },
           {
-            // This loader resolves url() and @imports inside CSS
-            loader: "css-loader"
-          },
-          {
-            // Then we apply postCSS fixes like autoprefixer and minifying
-            loader: "postcss-loader"
-          },
-          {
-            // First we transform SASS to standard CSS
             loader: "sass-loader",
             options: {
-              implementation: require("sass")
-            }
-          }
-        ]
-      },
-      //- Image Loader
-      {
-        // Now we apply rule for images
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            // Using file-loader for these files
-            loader: "file-loader",
-
-            // In options we can set different things like format
-            // and directory to save
-            options: {
-              outputPath: "images"
-            }
-          }
-        ]
-      },
-      //- Font files loader
-      {
-        // Apply rule for fonts files
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        use: [
-          {
-            // Using file-loader too
-            loader: "file-loader",
-            options: {
-              outputPath: "fonts"
+              sourceMap: true
             }
           }
         ]
       }
     ]
   },
+
   plugins: [
+    // extract css into dedicated file
     new MiniCssExtractPlugin({
-      filename: "bundle.css"
+      filename: "./assets/build/[name].min.css"
     })
   ],
 
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: "development",
-
-  watch: true
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      }),
+      // enable the css minification plugin
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 };
